27cd6c775de037154023ebf78a57bd1946adce1d
github username <github email address>
2020-04-10 09:12:35 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,29 +1,28 @@
+
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
 
-  attr_accessor :balance , :status 
-  attr_reader :name 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
-   def initialize(name)
-   @name = name 
-   @balance = 1000 
-   @status = "open" 
-   end 
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
 
-   def deposit(amount)
-    @balance = @balance + amount 
-   end 
+  def display_balance
+    "Your balance is $#{balance}."
+  end
 
-    def display_balance
-      "Your balance is $#{@balance}."
-    end 
+  def close_account
+    self.status = "closed"
+  end
 
-    def valid?
-         @status == "open" && @balance > 0 
-    end 
-
-    def close_account
-        @status = "closed"
-    end 
-
+  def valid?
+    balance > 0 && status == "open"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,48 +1,40 @@
-
-require 'pry'
-
 class Transfer
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
 
-  attr_accessor :status 
-  attr_reader :sender , :receiver , :amount 
-  
-  def initialize(sender, receiver , amount)
-    @sender = sender 
-    @receiver = receiver 
-    @amount = amount 
+  def initialize(sender, receiver, amount)
     @status = "pending"
-    @isvalid = self.valid?
-  end 
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 
-
-  def valid? 
-        # binding.pry 
-
-         self.sender.valid? && self.receiver.valid?  && @status == "pending" && self.sender.balance >= @amount
-         
-  end  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 
   def execute_transaction
-      if @isvalid 
-        self.sender.balance = self.sender.balance  - @amount 
-        self.receiver.balance = self.receiver.balance + @amount 
-        self.status = "complete"
-      else
-        self.status = "rejected"
-        "Transaction rejected. Please check your account balance."
-      end 
-  end 
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
 
-
-   def reverse_transfer 
-    if @status == "complete"
-      self.sender.balance = self.sender.balance  + @amount 
-      self.receiver.balance = self.receiver.balance - @amount 
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
       self.status = "reversed"
     else
-      "This Transfer cannot be reversed "
-    end 
-   end 
- 
+      reject_transfer
+    end
+  end
 
-end
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end 
